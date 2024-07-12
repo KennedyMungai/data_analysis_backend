8df5d86bd6a97c63f76c0406a4a2585834d9a695
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-07-12 15:39:16 +0300

    Set up kinde auth for the root endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 """The main file for the API"""
-from fastapi import FastAPI, status
+import os
+
+from dotenv import find_dotenv, load_dotenv
+from fastapi import Depends, FastAPI, status
+from kinde_sdk.kinde_api_client import KindeApiClient
+from starlette.middleware.sessions import SessionMiddleware
+
+from kinde.kinde import get_kinde_client
+
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 app = FastAPI(title='Data Analysis',
               description='The backend for a data analysis application',
               version='0.1.0'
               )
+
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
 
 @app.get(
@@ -13,10 +26,11 @@ app = FastAPI(title='Data Analysis',
     description='The root endpoint of the application',
     status_code=status.HTTP_200_OK
 )
-async def root():
+async def root(kinde_client: KindeApiClient = Depends(get_kinde_client)):
     """The root endpoint of the application
 
     Returns:
         dict: A simple message
     """
+    print(kinde_client)
     return {'Message': 'Hello World'}
